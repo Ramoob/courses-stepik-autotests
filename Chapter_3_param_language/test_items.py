9ceeb1f4a012c3db63a0b3dd771d6a6f11bdfc7a
Rm <git4mub@gmail.com>
2021-10-20 22:00:08 +0300

    Доработал assert

--- Chapter_3_param_language/test_items.py
+++ Chapter_3_param_language/test_items.py
@@ -6,5 +6,5 @@ import time
 def test_find_add_to_cart_button(browser):
 	browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
 	time.sleep(5)
-	button = browser.find_element_by_class_name('btn-add-to-basket').is_displayed()
-	assert button, "Кнопка 'Добавить в корзину' на странице присутствует"
+	button = browser.find_element_by_class_name('btn-add-to-basket')
+	assert button, "Кнопка 'Добавить в корзину' на странице отсутствует"
